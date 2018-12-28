f5bfdc803e412e19e1742edaeafbfe7e848e6db8
Mukund Sabne <mukundsabne811@gmail.com>
2018-12-28 17:52:48 +0530

    Created One Model class for jobs App

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'jobs.apps.JobsConfig',
+    'blogs.apps.BlogsConfig',
 ]
 
 MIDDLEWARE = [
