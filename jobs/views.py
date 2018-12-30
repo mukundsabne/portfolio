11fc45694ae7cd2b9a4d04cb3913a79cce0c4c4e
Mukund Sabne <mukundsabne811@gmail.com>
2018-12-30 13:38:01 +0530

    2018-12-30 commit

--- jobs/views.py
+++ jobs/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home(request):
+    return render(request,'index.html')
+
+def forajax(request):
+    return render(request,'home1.html')
--- portfolio/settings.py
+++ portfolio/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'portfolio.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,3 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+MEDIA_ROOT=  os.path.join(BASE_DIR, 'media')
+
+MEDIA_URL='media/'
